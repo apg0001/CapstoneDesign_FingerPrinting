19b9336e8f2ed5d911467b240a41a90f3dc6d1d6
박기찬 <apg0001naver@gmail.com>
2025-03-18 10:42:56 +0900

    Add encoder model storage and separate prediction code

--- codes/train.py
+++ codes/train.py
@@ -1,3 +1,4 @@
+import joblib
 import pandas as pd
 import chardet
 import numpy as np
@@ -13,6 +14,7 @@ from filterpy.kalman import KalmanFilter
 import datetime
 import glob
 import os
+from model import WifiCNN
 
 # 데이터 로드 및 인코딩 감지
 
@@ -161,31 +163,6 @@ class WifiDataset(Dataset):
 #         x = self.fc2(x)
 #         return x
 
-class WifiCNN(nn.Module):
-    def __init__(self, num_ap, num_classes, num_mac):
-        super(WifiCNN, self).__init__()
-        self.embedding = nn.Embedding(
-            num_embeddings=num_mac + 1,  # padding 포함
-            embedding_dim=8,
-            padding_idx=0  # padding index
-        )
-        self.conv1 = nn.Conv1d(
-            in_channels=9, out_channels=32, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv1d(32, 64, kernel_size=3, padding=1)
-        self.fc1 = nn.Linear(64 * num_ap, 128)
-        self.fc2 = nn.Linear(128, num_classes)
-        self.relu = nn.ReLU()
-
-    def forward(self, x, mac):
-        mac_embed = self.embedding(mac)
-        x = torch.cat([x.unsqueeze(2), mac_embed], dim=2)
-        x = x.permute(0, 2, 1)
-        x = self.relu(self.conv1(x))
-        x = self.relu(self.conv2(x))
-        x = x.view(x.size(0), -1)
-        x = self.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
 
 # 모델 학습 함수
 
@@ -216,9 +193,14 @@ def train_model(model, train_loader, test_loader, num_epochs=100):
             Loss=total_loss / len(train_loader), Accuracy=f"{train_accuracy:.2f}%")
 
     timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-    model_path = f"./models/fp_model_{timestamp}.pt"
-    torch.save(model.state_dict(), model_path)
-    print(f"모델 가중치 저장됨: {model_path}")
+    fp_model_path = f"./models/finger_printing/fp_model_{timestamp}.pt"
+    le_model_path = f"./models/location_encoder/location_encoder_{timestamp}.pkl"
+    me_model_path = f"./models/mac_encoder/mac_encoder_{timestamp}.pkl"
+    torch.save(model.state_dict(), fp_model_path)
+    # 학습 코드에서
+    joblib.dump(location_encoder, le_model_path)
+    joblib.dump(mac_encoder, me_model_path)
+    print(f"모델 가중치 저장됨: {fp_model_path}")
 
 # 모델 평가 함수
 
@@ -271,11 +253,11 @@ def predict_location(mac_rssi_dict, model_path):
 # 최신 모델 불러오기
 
 
-def get_latest_model():
-    model_files = glob.glob("./models/fp_model_*.pt")
-    if not model_files:
-        raise FileNotFoundError("저장된 모델이 없습니다.")
-    return max(model_files, key=os.path.getctime)
+# def get_latest_model():
+#     model_files = glob.glob("./models/fp_model_*.pt")
+#     if not model_files:
+#         raise FileNotFoundError("저장된 모델이 없습니다.")
+#     return max(model_files, key=os.path.getctime)
 
 
 # 메인 실행 코드
@@ -286,7 +268,7 @@ if __name__ == "__main__":
     X, y = create_dataset(df, mac_encoder)
 
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=42)
+        X, y, test_size=None, random_state=42)
     train_dataset = WifiDataset(X_train, y_train)
     test_dataset = WifiDataset(X_test, y_test)
     train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True)
@@ -298,11 +280,12 @@ if __name__ == "__main__":
 
     model = WifiCNN(num_ap, num_classes, num_mac)
     train_model(model, train_loader, test_loader, num_epochs=100)
-    evaluate_model(model, test_loader)
+    # evaluate_model(model, test_loader)
+    evaluate_model(model, train_loader)
 
-    # 예측 실행
-    test_input = {'26:3f:0b:e2:66:14': -40, '2c:3f:0b:e2:66:3f': -74, '2e:3f:1b:e2:64:73': -86, '26:3f:0b:e2:64:73': -75, '58:86:94:de:1b:78': -73, '2e:3f:1b:e2:66:14': -59, '22:3f:1b:e2:67:13': -89, '78:db:2f:0f:fb:06': -87, '12:3f:0b:59:2e:20': -87, '70:5d:cc:a3:86:58': -87, '78:db:2f:10:01:dd': -86, '78:db:2f:0f:f2:fd': -91, '2e:3f:1b:e2:66:b0': -77, '78:db:2f:0f:fe:a3': -85, '22:3f:1b:e2:66:3f': -77, '2a:3f:0b:e2:66:b0': -65, '26:3f:1b:59:2e:56': -87, '2a:3f:1b:e2:67:13': -89, 'ea:55:b8:6e:4b:9c': -87, '26:3f:0b:e2:66:b0': -65, '2a:3f:0b:e2:66:14': -40, '12:3f:0b:e2:67:13': -78, '58:86:94:16:4a:a8': -84, '58:86:94:de:10:e4': -59, 'e6:55:b8:6e:4b:9c': -87, '26:3f:0b:e2:65:61': -77, '82:de:4b:62:c7:ed': -76, '2a:3f:1b:59:2e:56': -87, '12:3f:1b:59:2e:56': -88, '82:de:4b:62:c7:ec': -76, '2a:3f:1b:e2:66:3f': -77, '36:3f:0b:e2:67:13': -67, '00:26:66:fe:8d:74': -77, '22:3f:0b:e2:66:b0': -66, '12:3f:1b:e2:66:b0': -77, '26:3f:1b:e2:66:3f': -77, '78:db:2f:10:04:59': -81, '36:3f:1b:e2:64:73': -86, '36:3f:0b:e2:66:3f': -75, '78:db:2f:0f:ff:d2': -92, '58:86:94:de:6c:84': -70, '22:3f:1b:59:2e:20': -84, '36:3f:1b:e2:66:14': -59, '2a:3f:0b:e2:67:13': -66, '36:3f:1b:e2:66:b0': -77, 'ee:55:b8:6e:4b:9c': -87, '78:db:2f:0f:fa:9a': -78, '36:3f:1b:59:2e:56': -88, '2c:3f:0b:e2:67:13': -70, '12:3f:1b:e2:66:14': -58, '22:3f:0b:e2:66:14': -40, '28:ee:52:de:60:ea': -
-                  85, '22:3f:0b:e2:64:73': -75, '2e:3f:1b:59:2e:56': -88, 'b2:ee:52:de:60:ed': -71, '2c:3f:0b:e2:66:b0': -68, 'f6:9e:38:74:f9:08': -88, '22:3f:1b:e2:66:b0': -77, 'c6:9e:38:74:f9:08': -88, '2e:3f:1b:e2:66:3f': -77, '2e:3f:1b:59:2e:20': -89, '26:3f:1b:59:2e:20': -83, '26:3f:0b:e2:67:13': -66, '12:3f:0b:e2:66:b0': -72, '2a:3f:1b:59:2e:20': -83, '22:3f:1b:e2:64:73': -86, '2a:3f:0b:e2:66:3f': -75, '22:3f:1b:e2:66:14': -59, '36:3f:0b:e2:65:61': -78, 'b4:fc:75:16:12:4f': -86, '78:db:2f:0f:fb:66': -74, '26:3f:0b:e2:66:3f': -75, '7a:de:4b:62:c7:ec': -89, '7a:de:4b:62:c7:eb': -88, '70:5d:cc:ca:e6:f4': -73, '12:3f:1b:e2:65:61': -92, '58:86:94:7e:ea:78': -91, '2a:3f:1b:e2:66:b0': -77, 'fe:55:b8:6e:4b:9c': -87, '12:3f:1b:e2:66:3f': -77, '22:3f:0b:e2:66:3f': -74, '58:86:94:55:20:d0': -88, '36:3f:1b:59:2e:20': -83, '2a:3f:1b:e2:64:73': -87, '70:5d:cc:cb:e6:f4': -67, '2a:3f:1b:e2:66:14': -59, '22:3f:0b:e2:67:13': -66, '26:3f:1b:e2:66:b0': -77, '36:3f:0b:e2:66:b0': -66, '26:3f:1b:e2:66:14': -59, '12:3f:1b:59:2e:20': -88, 'da:55:b8:6e:4b:9c': -86, '36:3f:0b:e2:66:14': -40, '36:3f:1b:e2:66:3f': -77, '26:3f:1b:e2:64:73': -87, '36:3f:0b:e2:64:73': -77, 'aa:ee:52:de:60:ec': -85, '70:5d:cc:60:cd:b0': -71, '78:db:2f:0f:fe:9b': -87, '36:3f:1b:e2:67:13': -90, '58:86:94:db:27:e4': -60, 'e2:55:b8:6e:4b:9c': -86, '22:3f:1b:59:2e:56': -88, '70:5d:cc:60:cd:b2': -89}
-    latest_model = get_latest_model()
-    predicted_location = predict_location(test_input, latest_model)
-    print(f"예측된 위치: {predicted_location}")
+    # # 예측 실행
+    # test_input = {'26:3f:0b:e2:66:14': -54, '2c:3f:0b:e2:66:3f': -59, 'da:55:a8:6e:4b:9c': -86, '26:3f:0b:e2:64:73': -77, '58:86:94:de:1b:78': -78, '70:5d:cc:a3:b1:fa': -90, '78:db:2f:0f:fb:06': -77, '26:3f:0b:e2:66:15': -86, '78:db:2f:10:01:dd': -77, '78:db:2f:0f:f2:fd': -88, '82:e8:2c:60:50:3a': -90, '2e:3f:1b:e2:66:b0': -85, '36:3f:0b:59:2e:20': -77, '26:3f:0b:59:2e:56': -74, '78:db:2f:0f:fe:a3': -79, '22:3f:1b:e2:66:3f': -70, '2c:3f:0b:e2:65:87': -79, '2a:3f:1b:e2:65:61': -87, '26:3f:1b:e2:65:61': -88, '22:3f:1b:e2:65:87': -88, '12:3f:0b:e2:66:3f': -58, '26:3f:1b:59:2e:56': -79, '2a:3f:0b:e2:64:73': -77, '12:3f:0b:e2:65:75': -83, '26:3f:0b:e2:66:b0': -69, '2a:3f:0b:e2:66:15': -87, '2a:3f:0b:e2:66:14': -55, '90:9f:33:69:18:98': -89, '12:3f:0b:e2:67:13': -81, '58:86:94:de:10:e4': -53, '58:86:94:99:68:8e': -74, '58:86:94:ae:06:de': -85, '22:3f:0b:59:2e:56': -74, '82:de:4b:62:c7:ed': -66, '2a:3f:1b:59:2e:56': -79, '12:3f:1b:59:2e:56': -79, '82:de:4b:62:c7:ec': -66, '2a:3f:1b:e2:66:3f': -70, '36:3f:0b:e2:67:13': -73, '36:3f:0b:e2:65:75': -79, '00:26:66:fe:8d:74': -80, '22:3f:0b:e2:66:b0': -68, '78:db:2f:0f:fa:ac': -79, '12:3f:1b:e2:66:b0': -84, '26:3f:1b:e2:66:3f': -71, 'e2:bb:9e:fd:51:93': -88, '78:db:2f:10:04:59': -79, '2a:3f:1b:e2:65:87': -89, 'e2:55:a8:6e:4b:9c': -85, '36:3f:1b:e2:64:73': -82, '36:3f:0b:e2:66:3f': -59, '78:db:2f:0f:ff:d2': -86, '58:86:94:de:6c:84': -72, '36:3f:1b:e2:66:14': -61, '2a:3f:0b:e2:67:13': -73, '2c:3f:0b:59:2e:20': -77, 'fe:55:a8:6e:4b:9c': -85, '22:3f:1b:e2:65:61': -88, '36:3f:1b:e2:66:b0': -85, '78:db:2f:0f:fa:9a': -65, '36:3f:1b:59:2e:56': -79, '1c:28:af:af:58:a2': -85, '26:3f:1b:e2:65:87': -89, '2c:3f:0b:e2:67:13': -73, '22:3f:0b:e2:66:15': -86, '22:3f:0b:e2:66:14': -
+    #               55, '28:ee:52:de:60:eb': -64, '28:ee:52:de:60:ea': -88, '22:3f:0b:e2:64:73': -77, '2e:3f:1b:59:2e:56': -79, '26:3f:0b:e2:65:87': -78, 'b2:ee:52:de:60:ed': -73, '36:3f:0b:e2:66:21': -81, 'ee:55:a8:6e:4b:9c': -84, '2a:3f:0b:59:2e:20': -77, 'f6:9e:38:74:f9:08': -86, '36:3f:1b:e2:65:61': -88, '2c:3f:0b:e2:66:15': -86, '22:3f:1b:e2:66:b0': -85, 'c6:9e:38:74:f9:08': -86, '2e:3f:1b:e2:66:3f': -70, '2c:3f:0b:e2:64:73': -77, '26:3f:0b:e2:67:13': -73, '22:3f:1b:e2:64:73': -82, '26:3f:0b:e2:65:75': -79, '12:3f:0b:59:2e:56': -74, '2a:3f:0b:e2:66:3f': -59, 'fa:9e:38:74:f9:08': -87, '22:3f:1b:e2:66:14': -61, '36:3f:0b:e2:65:61': -78, '00:04:ea:45:8a:14': -85, '26:3f:0b:e2:66:3f': -59, '78:db:2f:0f:fb:66': -74, '7a:de:4b:62:c7:ec': -88, '36:3f:0b:59:2e:56': -74, '7a:de:4b:62:c7:eb': -88, '70:5d:cc:ca:e6:f4': -74, '2a:3f:1b:e2:66:b0': -85, '12:3f:1b:e2:66:3f': -70, '22:3f:0b:e2:66:3f': -59, 'e4:55:a8:6e:4b:9c': -85, '2a:3f:1b:e2:64:73': -82, '70:5d:cc:cb:e6:f4': -67, '2a:3f:0b:e2:66:a9': -86, '36:3f:1b:e2:65:87': -88, '2a:3f:1b:e2:66:14': -61, '22:3f:0b:e2:67:13': -73, '22:3f:0b:e2:65:75': -78, '26:3f:1b:e2:66:b0': -85, '36:3f:0b:e2:66:b0': -70, '26:3f:1b:e2:66:14': -61, '12:3f:1b:59:2e:20': -91, '36:3f:0b:e2:66:15': -86, '36:3f:0b:e2:66:14': -55, '22:3f:0b:59:2e:20': -77, '36:3f:1b:e2:66:3f': -70, '2a:3f:0b:59:2e:56': -74, '26:3f:1b:e2:64:73': -82, 'aa:ee:52:de:60:ec': -87, 'ea:55:a8:6e:4b:9c': -85, '70:5d:cc:60:cd:b0': -70, '78:db:2f:0f:fe:9b': -67, '22:3f:0b:e2:65:87': -79, 'd4:6e:0e:a5:71:3c': -81, '58:86:94:db:27:e4': -62, '88:36:6c:69:7a:b2': -78, '1c:28:af:ae:f9:62': -88, '88:36:6c:87:0b:30': -94, '22:3f:1b:59:2e:56': -79, '40:ca:63:4e:41:b5': -80, '58:86:94:c9:8c:fa': -85, 'b8:62:1f:50:6e:5c': -87}
+    # latest_model = get_latest_model()
+    # predicted_location = predict_location(test_input, latest_model)
+    # print(f"예측된 위치: {predicted_location}")
