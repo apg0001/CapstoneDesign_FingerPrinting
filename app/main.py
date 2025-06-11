0c7a91b53e4679d8f3591d9d10ab990302eda437
박기찬 <apg0001naver@gmail.com>
2025-06-11 14:12:49 +0900

    모델 파일 업로드

--- app/main.py
+++ app/main.py
@@ -23,6 +23,10 @@ logger = logging.getLogger(__name__)
 # CONFIG_PATH = "./app/config/hyperparameters_20250423_181508.yaml"
 
 # 5층 데이터만
+# MODEL_PATH = "./app/checkpoints/fp_model_CNNTransformer_20250423_181508.pt"
+# ENCODER_PATH = "./app/checkpoints/encoders_20250423_181508.pkl"
+# NORM_PATH = "./app/checkpoints/norm_20250423_181508.pkl"
+# CONFIG_PATH = "./app/config/hyperparameters_20250423_181508.yaml"
 # MODEL_PATH = "./checkpoints/fp_model_CNNTransformer_20250423_181508.pt"
 # ENCODER_PATH = "./checkpoints/encoders_20250423_181508.pkl"
 # NORM_PATH = "./checkpoints/norm_20250423_181508.pkl"
@@ -35,10 +39,14 @@ logger = logging.getLogger(__name__)
 # CONFIG_PATH = "./app/config/hyperparameters_20250520_172109.yaml"
 
 # 6층 데이터 파인튜닝 후 증강
-MODEL_PATH = "./app/checkpoints/fp_model_CNNTransformer_20250608_143837.pt"
-ENCODER_PATH = "./app/checkpoints/encoders_20250608_143837.pkl"
-NORM_PATH = "./app/checkpoints/norm_20250608_143837.pkl"
-CONFIG_PATH = "./app/config/hyperparameters_20250608_143837.yaml"
+# MODEL_PATH = "./app/checkpoints/fp_model_CNNTransformer_20250608_143837.pt"
+# ENCODER_PATH = "./app/checkpoints/encoders_20250608_143837.pkl"
+# NORM_PATH = "./app/checkpoints/norm_20250608_143837.pkl"
+# CONFIG_PATH = "./app/config/hyperparameters_20250608_143837.yaml"
+MODEL_PATH = "./checkpoints/fp_model_CNNTransformer_20250608_143837.pt"
+ENCODER_PATH = "./checkpoints/encoders_20250608_143837.pkl"
+NORM_PATH = "./checkpoints/norm_20250608_143837.pkl"
+CONFIG_PATH = "./config/hyperparameters_20250608_143837.yaml"
 
 predictor = Predictor(MODEL_PATH, ENCODER_PATH, NORM_PATH, CONFIG_PATH)
 
